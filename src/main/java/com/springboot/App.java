a4abe30600898040f1826077f8ff8ee78a0ea284
759522451@qq.com <hanbei1993>
2018-03-18 21:18:12 +0800

    commit

--- src/main/java/com/springboot/App.java
+++ src/main/java/com/springboot/App.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableCaching
